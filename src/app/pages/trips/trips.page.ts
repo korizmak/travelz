4cc0c5d2eb0281a66523707e9cff0a69eb453497
Katarina Korizma <katarina.korizma@gmail.com>
2026-07-03 13:00:28 +0200

    Update trips page layout and style

--- src/app/pages/trips/trips.page.ts
+++ src/app/pages/trips/trips.page.ts
@@ -2,8 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { add } from 'ionicons/icons';
 import { Trip } from '../../models/trip.model';
 import { TravelDataService } from '../../services/travel-data.service';
+
+addIcons({ add });
 
 @Component({
   selector: 'app-trips',
